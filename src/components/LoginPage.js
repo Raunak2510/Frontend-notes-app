888195b036d8f5a121a8cdc3d0cabec290fc6f70
raunak2510 <raunakvhardwaj@gmail.com>
2025-04-18 03:04:56 +0530

    LoginPage.js Modified

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -21,12 +21,7 @@ function LoginPage({ setLoading }) {
       if (response.data.token) {
         localStorage.setItem('token', response.data.token);
         localStorage.setItem('username', username);
-        console.log('here')
-        if (localStorage.getItem('token')) {
-          navigate('/main');
-        }else {
-          navigate('/main');
-        }
+        navigate('/main');
       } else {
         setError(response.data.message || 'An unexpected error occurred');
       }
